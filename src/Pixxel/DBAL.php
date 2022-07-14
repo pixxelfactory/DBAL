65d0e624bbebf0383a3df993d4371169b86b3d77
Alex Franzelin <mail@alex-franzelin.it>
2022-07-14 11:28:45 +0200

    Add methods to check if a table and / or column exists, add a method to check if pdo is connected and methods to get all columns and tables

--- src/Pixxel/DBAL.php
+++ src/Pixxel/DBAL.php
@@ -14,10 +14,11 @@ class Dbal
     protected $password;
     protected $database;
     protected $host;
-    protected $pdo;
+    protected $pdo = false;
     protected $lastQuery;
     protected $lastError;
-    public function __construct($username, $password, $database, $host = 'localhost')
+
+    public function __construct($username = false, $password = false, $database = false, $host = 'localhost')
     {
         $this->username = $username;
         $this->password = $password;
@@ -25,22 +26,136 @@ class Dbal
         $this->host = $host;
         $this->lastQuery = false;
         $this->lastError = false;
-    // I really hate exceptions
+
+        // I really hate exceptions
         try {
             $this->pdo = new \PDO('mysql:host=localhost;dbname=' . $database, $username, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
         } catch (\PDOException $e) {
             $this->lastError = $e;
+            $this->pdo = false;
+
             return false;
         }
+    }
+
+    /**
+     * Manually set a raw pdo object, if you have already a pdo connection, you can set it here
+     * @param object $pdo
+     */
+    public function setPdo(\PDO $pdo)
+    {
+        $this->pdo = $pdo;
     }
 
     /**
      * Get the raw pdo object
      * @return object
      */
-    public function getPdo()
+    public function getPdo(): bool|object
     {
         return $this->pdo;
+    }
+
+    /**
+     * Check if there is a connection to a db
+     * @return bool
+     */
+    public function isConnected(): bool
+    {
+        return $this->pdo instanceof \PDO ? true : false;
+    }
+
+    /**
+     * Get a list of all tables in the current db, returns them as array, false if the command failed
+     * @return array|bool
+     */
+    public function getTables(): array|bool
+    {
+        $tables = $this->read('show tables', [], true);
+        $return = [];
+        
+        // If something went wrong, retur nfalse
+        if(!is_array($tables)) {
+            return false;
+        }
+
+        // Convert all rows to a plain array and get the first value
+        foreach($tables as $table) {
+            $return[] = reset($table);
+        }
+
+        return $return;
+    }
+
+    /**
+     * Get the column names from a table and return them as array
+     * Make sure to whitelist the table name first!
+     * @param string $table
+     * @param bool $checkTableName=true     If set to true, it will check if the table really exists to prevent sql-injections, if you checked it beforehand, disable it
+     * @param string $onlyNames=true        If set to true, only a simple array with the table names will be returned, otherwise a 2dimensional array containing type ecc
+     * @return array|bool
+     */
+    public function getColumns(string $table, $checkTableName = true, $onlyNames = true): array|bool
+    {
+        // If checkTableName is set to true, get a list of all tables in the current db and whitelist the table-name provided
+        if($checkTableName == true) {
+            $tables = $this->getTables();
+
+            if(!$tables || !in_array($table, $tables)) {
+                return false;
+            }
+        }
+
+        $columns = $this->read('show columns from `'.$table.'`', []);
+        $return = [];
+
+        if(!is_array($columns)) {
+            return false;
+        }
+
+        // If we want further information like length, type and so on, $onlyNames must be set to false
+        if(!$onlyNames) {
+            return $columns;
+        }
+
+        foreach($columns as $column) {
+            $return[] = $column->Field;
+        }
+
+        return $return;
+    }
+
+    /**
+     * Checks if a table really exists in the current database
+     * @param string $table
+     * @return bool
+     */
+    public function tableExists(string $table): bool
+    {
+        $existingTables = $this->getTables();
+
+        if(!in_array($table, $existingTables)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Check if a column exists / is valid in a certain table
+     * @param string $column
+     * @param string $table
+     * @return bool
+     */
+    public function columnExists(string $column, string $table): bool
+    {
+        $existingColumns = $this->getColumns($table);
+
+        if(!in_array($column, $existingColumns)) {
+            return false;
+        }
+
+        return true;
     }
 
     /**
@@ -53,6 +168,7 @@ class Dbal
     {
         $stmt = $this->pdo->prepare($query);
         $this->lastQuery = $query;
+
         if (!$stmt->execute($parameters)) {
             $this->error = $stmt->errorInfo();
             return false;
@@ -65,22 +181,32 @@ class Dbal
      * Perform a select query and transform result into a php-array for easier access, false if nothing comes back
      * @param string $query
      * @param array $parameters
+     * @param bool $fetchAsArray=false      If set to true, it will fetch arrays instead of objects
      * @return array|bool
      */
-    public function read($query, $parameters = [])
+    public function read($query, $parameters = [], $fetchAsArray = false)
     {
         $stmt = $this->pdo->prepare($query);
         $this->lastQuery = $query;
+
         if (!$stmt->execute($parameters)) {
             $this->error = $stmt->errorInfo();
             return false;
         }
 
         $return = [];
-        while ($row = $stmt->fetchObject()) {
-            $return[] = $row;
-        }
 
+        if($fetchAsArray == true) {
+            while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+                $return[] = $row;
+            }
+        }
+        else {
+            while ($row = $stmt->fetchObject()) {
+                $return[] = $row;
+            }
+        }
+        
         return $return;
     }
 
@@ -93,6 +219,7 @@ class Dbal
     public function readSingle($query, $parameters = []): object|bool
     {
         $result = $this->read($query, $parameters);
+        
         return !empty($result) ? $result[0] : false;
     }
 
